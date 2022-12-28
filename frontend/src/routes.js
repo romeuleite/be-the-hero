d44267c6f6b8c856cf75a77b68b4b9dd7238b2a8
Romeu Leite <romeufilho@estudante.ufscar.br>
2022-12-27 21:13:33 -0300

    Mobile-do-app

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -3,6 +3,8 @@ import { BrowserRouter, Routes, Route} from 'react-router-dom';
 
 import Logon from './pages/Logon';
 import Register from './pages/Register';
+import Profile from './pages/Profile';
+import NewIncident from './pages/NewIncident';
 
 export default function Rotas(){
     return (
@@ -10,6 +12,9 @@ export default function Rotas(){
             <Routes>
                 <Route path='/' exact element = {<Logon />}/>
                 <Route path='/register' element = {<Register />}/>
+                <Route path='/profile' element = {<Profile />}/>
+                <Route path='/incidents/new' element = {<NewIncident />}/>
+
             </Routes>
         </BrowserRouter>
     );
---
+++ mobile/App.js
@@ -0,0 +1,24 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Nelson Rubens</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#7159c1',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+
+  title:{
+    color: '#FFF',
+  }
+});
