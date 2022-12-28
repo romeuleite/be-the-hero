1e9437ef2873264281b7d64fb7e739779bdf1ae1
Romeu Leite <romeufilho@estudante.ufscar.br>
2022-12-28 17:16:24 -0300

    Att-mobilde-do-app

--- mobile/App.js
+++ mobile/App.js
@@ -1,24 +1,27 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+import 'intl';
+import 'intl/locale-data/jsonp/pt-BR'; // or any other locale you need
+
+import Routes from './src/routes';
+
+
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Nelson Rubens</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Routes />
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#7159c1',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#7159c1',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
 
-  title:{
-    color: '#FFF',
-  }
-});
+//   title:{
+//     color: '#FFF',
+//   }
+// });
